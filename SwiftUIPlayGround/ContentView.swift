a65d6efa017100e09e07be16228cef5141dd1a3f
narumichikubo <narumichi7373@gmail.com>
2022-03-12 18:00:52 +0900

    Initial Commit

---
+++ SwiftUIPlayGround/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIPlayGround
+//
+//  Created by Narumichi Kubo on 2022/03/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIPlayGround/SwiftUIPlayGroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPlayGroundApp.swift
+//  SwiftUIPlayGround
+//
+//  Created by Narumichi Kubo on 2022/03/12.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPlayGroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
